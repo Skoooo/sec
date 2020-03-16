f4edf8ad56efacf81bf144062fe84e26571fb250
skoo <yeoun8362@naver.com>
2020-03-16 19:50:36 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), # ''문자열일때 post_list를 보여줌
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 
 # url 설정 https://docs.djangoproject.com/en/2.0/topics/http/urls/
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,4 +7,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts}) # requset를 받아 render 메서드 호출, blog/post_list.html 출력
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 # 쿼리셋 공식문서 https://docs.djangoproject.com/en/2.0/ref/models/querysets/
